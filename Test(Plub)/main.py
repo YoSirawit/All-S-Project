9f63a7eb0333a2fe90d3f2ceb4369952c70a4865
unknown <66070314@kmitl.ac.th>
2023-10-08 11:13:11 +0700

    retrieve shopname from database and display

--- Test(Plub)/main.py
+++ Test(Plub)/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from database import shopname
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Home.html")
+    shopnames = shopname()
+    return render_template("Home.html", Shopnames = shopnames)
 
 @app.route("/login")
 def login():
