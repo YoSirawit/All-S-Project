1b961c989c6c5b050a8784c22c611b337171373b
unknown <66070314@kmitl.ac.th>
2023-10-09 21:41:13 +0700

    add display order and insert user into database

--- Test(Plub)/database.py
+++ Test(Plub)/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, text
 from flask_mysqldb import MySQL
 
-db_connection_string = "mysql+pymysql://84vq5ilryqpjtctnvf28:pscale_pw_T8kgj3hNDost8moUhm8DYWbOCZuyXZLgbyeLwJQInLL@aws.connect.psdb.cloud/itrestaurant?charset=utf8mb4"
+db_connection_string = ""
 
 engine = create_engine(
     db_connection_string, 
@@ -24,3 +24,13 @@ def shopname():
         shopnames = conn.execute(text("select shopname from shoplist"))
 
     return shopnames.all()
+
+def orders(shopnames):
+    with engine.connect() as conn:
+        order = conn.execute(text("SELECT menu FROM orders WHERE shopname = \"{0}\"".format(shopnames)))
+
+    return order.all()
+
+def add_data(user_name, type_user, phone):
+    with engine.connect() as conn:
+        conn.execute(text("INSERT INTO userid(username, typeuser, telephone) VALUES('{0}', {1}, '{2}');".format(user_name, type_user, phone)))
