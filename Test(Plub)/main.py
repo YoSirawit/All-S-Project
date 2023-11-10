cd97d55a42ab0956456678f5dd0c1fc3ccedc3d4
YoSirawit <66070314@kmitl.ac.th>
2023-11-10 15:38:24 +0700

    pass usertype to html

--- Test(Plub)/main.py
+++ Test(Plub)/main.py
@@ -18,7 +18,7 @@ USER_LST = load_userid_from_db()
 @app.route("/home")
 def home():
     shopnames = shopname()
-    return render_template("home.html", Shopnames = shopnames, username= session['username'])
+    return render_template("home.html", Shopnames = shopnames, username= session['username'], usertype = session['usertype'])
 
 @app.route("/", methods=['GET', 'POST'])
 def login():
@@ -44,6 +44,7 @@ def login():
                     session["loggedin"]=True
                     session['username']=user[1]
                     session['userid']=user[0]
+                    session['usertype']=user[2]
                     return redirect(url_for('home'))
     return render_template("login.html")
 
@@ -52,6 +53,7 @@ def logout():
     session.pop('loggedin', None)
     session.pop('username', None)
     session.pop('userid', None)
+    session.pop('usertype', None)
     return redirect(url_for("login"))
 
 @app.route("/shops/<shopnames>", methods=['GET', 'POST'])
@@ -64,7 +66,7 @@ def shoppage(shopnames):
         note = request.form.get('note')
         add_menu(user, time, menu, shopnames)
         # cursor.execute(f"INSERT INTO orders(username, time_want, menu, shopname) VALUES('{user}', {time}, '{menu}', '{note}')")
-    return render_template("shoppage.html", Shopnames = shopnames, Order = order, username= session['username'])
+    return render_template("shoppage.html", Shopnames = shopnames, Order = order, username= session['username'], usertype = session['usertype'])
 
 @app.route("/help")
 def help():
@@ -72,7 +74,7 @@ def help():
     cursor.execute(f"SELECT shopname FROM shoplist WHERE shopownerid = {shopowner}")
     shopname = cursor.fetchone()
     order = orders(shopname[0])
-    return render_template("order_list.html", Order = order, userid = session['userid'])
+    return render_template("order_list.html", Order = order, userid = session['userid'], usertype = session['usertype'])
 
 if  __name__ == "__main__":
     app.run(debug=True)
